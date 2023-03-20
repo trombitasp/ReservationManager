319819efd5493838cd4d077d9e9f7d1dc31c5ae2
trombitasp <78873457+trombitasp@users.noreply.github.com>
2023-03-20 20:41:09 +0100

    initial

---
+++ ReservationManager/ReservationManager/src/main/kotlin/trombitasp/ReservationManager/ReservationManagerApplication.kt
@@ -0,0 +1,11 @@
+package trombitasp.ReservationManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReservationManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReservationManagerApplication>(*args)
+}
---
+++ ReservationManager/ReservationManager/src/test/kotlin/trombitasp/ReservationManager/ReservationManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package trombitasp.ReservationManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservationManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
