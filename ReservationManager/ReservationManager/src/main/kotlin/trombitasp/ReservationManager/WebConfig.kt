c7cb2eb5f107824b79df35be36653b084343d624
trombitasp <trombitasp@gmail.com>
2023-05-01 22:20:16 +0200

    Update WebConfig.kt

--- ReservationManager/ReservationManager/src/main/kotlin/trombitasp/ReservationManager/WebConfig.kt
+++ ReservationManager/ReservationManager/src/main/kotlin/trombitasp/ReservationManager/WebConfig.kt
@@ -5,11 +5,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@Configuration
+/*@Configuration
 @EnableWebMvc
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
     }
 
-}
+}*/
