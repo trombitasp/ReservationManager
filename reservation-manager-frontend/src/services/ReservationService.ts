c41746b4a5f18525bd1b64efa5f15c81421183ae
trombitasp <trombitasp@gmail.com>
2023-05-12 14:29:17 +0200

    Update ReservationService.ts

--- reservation-manager-frontend/src/services/ReservationService.ts
+++ reservation-manager-frontend/src/services/ReservationService.ts
@@ -2,29 +2,33 @@ import http from "../util/util";
 import IReservationModel from "../models/ReservationModel"
 
 class ReservationDataService {
-    getAll() {
-      return http.get<Array<IReservationModel>>("/reservations");
-    }
-  
-    findById(id: string) {
-      return http.get<IReservationModel>(`/reservations/${id}`);
-    }
-  
-    create(data: IReservationModel) {
-      return http.post<IReservationModel>("/reservations", data);
-    }
-  
-    update(data: IReservationModel, id: Number) {
-      return http.put<any>(`/reservations/${id}`, data);
-    }
-  
-    delete(id: Number) {
-      return http.delete<any>(`/reservations/${id}`);
-    }
-  
-    /*deleteAll() {
-      return http.delete<any>(`/reservations`);
-    }*/
-  }
-  
-  export default new ReservationDataService();
+	getAll() {
+		return http.get<Array<IReservationModel>>("/reservations");
+	}
+
+	findById(id: string) {
+		return http.get<IReservationModel>(`/reservations/${id}`);
+	}
+
+	findByUserId(userId: string) {
+		return http.get<Array<IReservationModel>>(`/reservations/byuser/${userId}`);
+	}
+
+	create(data: IReservationModel) {
+		return http.post<IReservationModel>("/reservations", data);
+	}
+
+	update(data: IReservationModel, id: Number) {
+		return http.put<any>(`/reservations/${id}`, data);
+	}
+
+	delete(id: Number) {
+		return http.delete<any>(`/reservations/${id}`);
+	}
+
+	/*deleteAll() {
+	  return http.delete<any>(`/reservations`);
+	}*/
+}
+
+export default new ReservationDataService();
